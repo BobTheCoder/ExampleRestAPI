4a79af19ff1d29bed5e43d0f653827273bcf4224
Robert Craske <rob_craske@hotmail.com>
2019-03-18 20:31:19 +0000

    Load products

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,50 +4,23 @@ values('VqKb4tyj9V6i','Shield', 1149);
 insert into products
 values('DXSQpv6XVeJm','Helmet', 999);
 
--- [
---   {
---     "id": "VqKb4tyj9V6i",
---     "name": "Shield",
---     "usdPrice": 1149
---   },
---   {
---     "id": "DXSQpv6XVeJm",
---     "name": "Helmet",
---     "usdPrice": 999
---   },
---   {
---     "id": "7dgX6XzU3Wds",
---     "name": "Sword",
---     "usdPrice": 899
---   },
---   {
---     "id": "PKM5pGAh9yGm",
---     "name": "Axe",
---     "usdPrice": 799
---   },
---   {
---     "id": "7Hv0hA2nmci7",
---     "name": "Knife",
---     "usdPrice": 349
---   },
---   {
---     "id": "500R5EHvNlNB",
---     "name": "Gold Coin",
---     "usdPrice": 249
---   },
---   {
---     "id": "IP3cv7TcZhQn",
---     "name": "Platinum Coin",
---     "usdPrice": 399
---   },
---   {
---     "id": "IJOHGYkY2CYq",
---     "name": "Bow",
---     "usdPrice": 649
---   },
---   {
---     "id": "8anPsR2jbfNW",
---     "name": "Silver Coin",
---     "usdPrice": 50
---   }
--- ]
+insert into products
+values('7dgX6XzU3Wds','Sword', 899);
+
+insert into products
+values('PKM5pGAh9yGm','Axe', 799);
+
+insert into products
+values('7Hv0hA2nmci7','Knife', 349);
+
+insert into products
+values('500R5EHvNlNB','Gold Coin', 249);
+
+insert into products
+values('IP3cv7TcZhQn','Platinum Coin', 399);
+
+insert into products
+values('IJOHGYkY2CYq','Bow', 649);
+
+insert into products
+values('8anPsR2jbfNW','Silver Coin', 50);
